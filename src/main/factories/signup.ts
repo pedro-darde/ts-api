7ad70ae4c88200c43664cb2d47f3c4491f9d93af
pedro-darde <pedro.dardepedro-darde@hotmail.com>
2021-10-05 20:36:16 -0300

    feat: add SignUp factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,13 @@
+import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter'
+import { AccountMongoRepository } from '@/infra/db/mongodb/account-repository/account'
+import { SignUpController } from '@/presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '@/utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+export const makeSingUpController = (): SignUpController => {
+  const salt = 12
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const addAccountRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, addAccountRepository)
+  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+}
