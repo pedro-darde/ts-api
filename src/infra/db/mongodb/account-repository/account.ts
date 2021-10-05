a4ba132efcd3f8fa335a99e2cff9f02325586d27
pedro-darde <pedro.dardepedro-darde@hotmail.com>
2021-10-04 22:36:52 -0300

    ensure AccountMongoRepository returns an account on sucess

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,14 @@
+import { AccountModel } from '@/domain/models/account'
+import { AddAccountModel } from '@/domain/usecases/add-account'
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const { insertedId } = await accountCollection.insertOne(accountData)
+    const result = await accountCollection.findOne(insertedId)
+    const { _id, ...accountWihtouId } = result
+    /* @ts-expect-error */
+    return Object.assign({}, accountWihtouId, { id: _id })
+  }
+}
