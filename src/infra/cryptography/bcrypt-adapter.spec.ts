59487b169b956c9063a05e659f2e1205aab42ee7
pedro-darde <pedro.dardepedro-darde@hotmail.com>
2021-10-14 22:26:34 -0300

    feat: ensure bcrypt adaptare throws if compare throw

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -28,7 +28,7 @@ describe('Bcrypt Adapter', () => {
     expect(hashedPass).toBe('hash')
   })
 
-  test('Should throw if bcrypt throws', async () => {
+  test('Should throw if hash throws', async () => {
     const sut = makeSut()
     /** @ts-expect-error */
     jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
@@ -43,17 +43,25 @@ describe('Bcrypt Adapter', () => {
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
 
-  test('Should return a true when compare succeeds', async () => {
+  test('Should return true when compare succeeds', async () => {
     const sut = makeSut()
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(true)
   })
 
-  test('Should return a false when compare fails', async () => {
+  test('Should return false when compare fails', async () => {
     const sut = makeSut()
     /** @ts-expect-error */
     jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise(resolve => resolve(false)))
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(false)
+  })
+
+  test('Should throw if compare throws', async () => {
+    const sut = makeSut()
+    /** @ts-expect-error */
+    jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.compare('any_value', 'any_hash')
+    await expect(promise).rejects.toThrow()
   })
 })
