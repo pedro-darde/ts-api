9188d517d81306b55f224d4f79a1bf1ee0b73a0b
pedro-darde <pedro.dardepedro-darde@hotmail.com>
2021-10-14 22:24:34 -0300

    feat: ensure bcrypt adapter returns false when compare succeeds

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -45,7 +45,15 @@ describe('Bcrypt Adapter', () => {
 
   test('Should return a true when compare succeeds', async () => {
     const sut = makeSut()
-    const hashedPass = await sut.compare('any_value', 'any_hash')
-    expect(hashedPass).toBe(true)
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(true)
+  })
+
+  test('Should return a false when compare fails', async () => {
+    const sut = makeSut()
+    /** @ts-expect-error */
+    jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise(resolve => resolve(false)))
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(false)
   })
 })
