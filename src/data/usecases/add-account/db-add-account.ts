43de8d4493c669996a75c9e723f44781eb999ac6
pedro-darde <pedro.dardepedro-darde@hotmail.com>
2021-10-18 21:08:49 -0300

    refactor: move util files to main layer

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,11 +1,9 @@
 import { AddAccount, AddAccountModel, AccountModel, Hasher, AddAccountRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
-  private readonly encrypter: Hasher
-  private readonly addAccountRepository: AddAccountRepository
-  constructor (encrypter: Hasher, addAccountRepository: AddAccountRepository) {
-    this.encrypter = encrypter
-    this.addAccountRepository = addAccountRepository
+  constructor (
+    private readonly encrypter: Hasher,
+    private readonly addAccountRepository: AddAccountRepository) {
   }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
