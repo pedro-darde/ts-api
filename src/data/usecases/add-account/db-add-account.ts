1c27b9412e5cbf950e5307123bd6d2079d9ea34a
pedro-darde <pedro.dardepedro-darde@hotmail.com>
2021-11-02 19:47:34 -0300

    feat: ensure db-add-account calls loadAccountByEmailRepository

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,12 +1,15 @@
 import { AddAccount, AddAccountModel, AccountModel, Hasher, AddAccountRepository } from './db-add-account-protocols'
+import { LoadAccountByEmailRepository } from '../../protocols/db/account/load-account-by-email-repository'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly encrypter: Hasher,
-    private readonly addAccountRepository: AddAccountRepository) {
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {
   }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
     const hashedPassword = await this.encrypter.hash(accountData.password)
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
     return account
