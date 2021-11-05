ca68fc70a539b7880b7d99cb61584da53bf1507d
pedro-darde <pedro.dardepedro-darde@hotmail.com>
2021-11-04 23:35:08 -0300

    feat: add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,35 @@
+import { Collection } from 'mongodb'
+import app from '../config/app'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+
+let surveyCollection: Collection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('Should return 204 on add survey', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer1',
+            image: 'https://image-name.com'
+          }]
+        }).expect(204)
+    })
+  })
+})
