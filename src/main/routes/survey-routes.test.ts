354035facd4cb13b89410738cff1f22c318fea9f
pedro-darde <pedro.dardepedro-darde@hotmail.com>
2021-11-09 22:30:56 -0300

    fix: ensure accountMongoRepository allows admin to access any route

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import { Collection } from 'mongodb'
 import app from '../config/app'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import jwt from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -16,7 +19,9 @@ describe('Survey Routes', () => {
 
   beforeEach(async () => {
     surveyCollection = MongoHelper.getCollection('surveys')
+    accountCollection = MongoHelper.getCollection('accounts')
     await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
@@ -30,6 +35,28 @@ describe('Survey Routes', () => {
             image: 'https://image-name.com'
           }]
         }).expect(403)
+    })
+
+    test('Should return 204 with valid token', async () => {
+      const { insertedId } = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        role: 'admin'
+      })
+
+      const token = jwt.sign({ id: insertedId }, env.jwtSecret)
+      await accountCollection.updateOne({ _id: insertedId }, { $set: { accessToken: token } })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', token)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer1',
+            image: 'https://image-name.com'
+          }]
+        }).expect(204)
     })
   })
 })
