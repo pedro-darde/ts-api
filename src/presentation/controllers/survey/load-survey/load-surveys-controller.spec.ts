577ace6c4b20afcf840f6bbab0a0c8c8f578649a
pedro-darde <pedro.dardepedro-darde@hotmail.com>
2021-11-24 19:02:43 -0300

    test: ensure loadController return 500 if LoadSurvey throws

--- src/presentation/controllers/survey/load-survey/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-survey/load-surveys-controller.spec.ts
@@ -1,7 +1,7 @@
 import { LoadSurveysController } from './load-surveys-controller'
 import { LoadSurveys, SurveyModel } from './load-surveys-controller-protocols'
 import MockDate from 'mockdate'
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, serverError } from '../../../helpers/http/http-helper'
 
 const makeFakeSurveys = (): SurveyModel[] => {
   return [{
@@ -69,5 +69,11 @@ describe('LoadSurveysController', () => {
     } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(ok(makeFakeSurveys()))
+  })
+  test('Should return 500 if LoadSurvey throws', async () => {
+    const { sut, loadSurveysStub } = makeSut()
+    jest.spyOn(loadSurveysStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
